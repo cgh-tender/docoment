482ccfb55c764911bc09cb253fe88f641f320115
Haidar-cgh <late_tender@163.com>
2020-01-03 09:59:55 +0800

    调整

--- src/main/java/cn/com/SpringBootDomeApplication.java
+++ src/main/java/cn/com/SpringBootDomeApplication.java
@@ -2,17 +2,20 @@ package cn.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @ServletComponentScan("cn.com")
+@EnableConfigurationProperties
 public class SpringBootDomeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDomeApplication.class, args);
-	}
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootDomeApplication.class, args);
+    }
 
 
 
--- src/main/java/cn/com/utils/AuthFilterItemProperties.java
+++ src/main/java/cn/com/utils/AuthFilterItemProperties.java
@@ -1,15 +1,13 @@
 package cn.com.utils;
 
 import cn.com.utils.interfaceRun.YmlPropertySourceComponent;
-import cn.com.utils.interfaceRun.YmlPropertySourceProperties;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-@YmlPropertySourceComponent(value = {"classpath:/FilterItem/AuthFilter.yml"})
-@YmlPropertySourceProperties("data.item")
+@YmlPropertySourceComponent(value = {"classpath:/FilterItem/AuthFilter.yml"},name = "data.item")
 @Data
 public class AuthFilterItemProperties {
     private List<String> item;
