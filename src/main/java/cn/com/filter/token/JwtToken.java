f014ef49afed4ec603b40cf0248585440ff4d09e
HaiDar-cgh <guohai@160090>
2020-01-13 01:08:19 +0800

    部分调整内容.

--- src/main/java/cn/com/filter/token/JwtToken.java
+++ src/main/java/cn/com/filter/token/JwtToken.java
@@ -2,12 +2,6 @@ package cn.com.filter.token;
 
 import cn.com.SpringContextUtil;
 import com.alibaba.fastjson.JSONObject;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTCreator;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.impl.JWTParser;
-import com.auth0.jwt.interfaces.Claim;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.log4j.Log4j;
 import org.apache.commons.lang3.StringUtils;
 
@@ -18,64 +12,22 @@ import java.util.List;
 
 @Log4j
 public class JwtToken implements Serializable,JwtSuper {
-
     private final static String alg = "SHA256"; //HMAC SHA256
     private final static String typ = "JWT";
     private final static String ClaimName = "TokenPayloadAbs";
     private static final Long serialVersionUID = SpringContextUtil.serialVersionUID;
 
-    public String createUserNameJwtToken(TokenUserNamePayload payload) throws Exception {
-        return createJwtToken(payload,getAlgorithm(payload.getUserName()));
-    }
-
-    public String createUserPhoneJwtToken(TokenUserPhonePayload payload) throws Exception {
-        return createJwtToken(payload,getAlgorithm(payload.getPhone()));
-    }
-
     @Override
-    public String createJwtToken(TokenPayloadAbs payload,Algorithm secret) throws Exception {
-        JWTCreator.Builder builder = JWT.create();
-        HashMap<String, Object> header = getHeader();
-        Long uuid = payload.getUUID();
-        builder.withAudience(JSONObject.toJSONString(payload));
-        builder.withClaim(ClaimName,JSONObject.toJSONString(payload));
-        builder.withHeader(header);
-        builder.withJWTId(String.valueOf(uuid));
-        Date date = new Date();
-        builder.withIssuedAt(date);
-        builder.withNotBefore(date);
-        builder.withExpiresAt(new Date(date.getTime() + 10000));
-        return builder.sign(secret);
-    }
-
-    /**
-     * 获取 getAlgorithm 加密 code 是当前唯一值
-     * @param code 唯一值
-     * @return Algorithm
-     * @throws Exception 返回错误类型写回前台
-     */
-    private static Algorithm getAlgorithm(String code) throws Exception {
-        final String error = "当前Token不支持当前的加密方式[%s]请联系管理员";
-        if (StringUtils.equals(alg,"SHA256")){
-            return Algorithm.HMAC256(SpringContextUtil.TOKENSALT.getBytes());
-        }else if (StringUtils.equals(alg,"HMAC")){
-            return Algorithm.HMAC256(SpringContextUtil.TOKENSALT.getBytes());
-        }else {
-            throw new Exception(String.format(error,alg));
-        }
-    }
-
-    public static  <T> T getTokenPayloadAbs(String token){
-        DecodedJWT decode = JWT.decode(token);
-        String header = decode.getHeader();
-        Claim claim = decode.getClaim(ClaimName);
-        List<String> audience = decode.getAudience();
-        log.info(audience);
+    public String createJwtToken(TokenPayloadAbs payload) throws Exception {
         return null;
     }
 
+    public static  <T> T getTokenPayloadAbs(String token){
+       return null;
+    }
+
     public static String getTokenUUID(String token){
-        return JWT.decode(token).getId();
+       return null;
     }
 
     private static HashMap<String,Object> getHeader(){
@@ -84,10 +36,14 @@ public class JwtToken implements Serializable,JwtSuper {
         header.put("typ",typ);
         return header;
     }
+    private static HashMap<String,Object> getSecretKey(){
+
+        return null;
+    }
 }
 
 interface JwtSuper{
-    String createJwtToken(TokenPayloadAbs payload,Algorithm secret) throws Exception;
+    String createJwtToken(TokenPayloadAbs payload) throws Exception;
 }
 
 enum JwtType{
--- src/test/java/cn/com/filter/token/JwtTokenTest.java
+++ src/test/java/cn/com/filter/token/JwtTokenTest.java
@@ -18,20 +18,20 @@ class JwtTokenTest {
 
     @Test
     void main() {
-        request = new MockHttpServletRequest();
-        request.setCharacterEncoding("UTF-8");
-        TokenUserNamePayload admin = new TokenUserNamePayload("admin", request);
-        JwtToken jwtToken = new JwtToken();
-        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ7XCJJUFwiOlwiMTI3LjAuMC4xXCIsXCJVVUlEXCI6MTIxNjMxNzc5MDkwMjY0NDczNixcImlQXCI6XCIxMjcuMC4wLjFcIixcInNlcmlhbFZlcnNpb25VSURcIjoyNCxcInR5cGVcIjpcIlVTRVJOQU1FXCIsXCJ1VUlEXCI6MTIxNjMxNzc5MDkwMjY0NDczNixcInVzZXJOYW1lXCI6XCJhZG1pblwifSIsIm5iZiI6MTU3ODgyNzczNCwiVG9rZW5QYXlsb2FkQWJzIjoie1wiSVBcIjpcIjEyNy4wLjAuMVwiLFwiVVVJRFwiOjEyMTYzMTc3OTA5MDI2NDQ3MzYsXCJpUFwiOlwiMTI3LjAuMC4xXCIsXCJzZXJpYWxWZXJzaW9uVUlEXCI6MjQsXCJ0eXBlXCI6XCJVU0VSTkFNRVwiLFwidVVJRFwiOjEyMTYzMTc3OTA5MDI2NDQ3MzYsXCJ1c2VyTmFtZVwiOlwiYWRtaW5cIn0iLCJleHAiOjE1Nzg4Mjc3NDQsImlhdCI6MTU3ODgyNzczNCwianRpIjoiMTIxNjMxNzc5MDkwMjY0NDczNiJ9.t4po2hDvtyTKX-47Eh8Wf8dwKZcV8uWA3ZQftEelO7s";
+//        request = new MockHttpServletRequest();
+//        request.setCharacterEncoding("UTF-8");
+//        TokenUserNamePayload admin = new TokenUserNamePayload("admin", request);
+//        JwtToken jwtToken = new JwtToken();
+//        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ7XCJJUFwiOlwiMTI3LjAuMC4xXCIsXCJVVUlEXCI6MTIxNjMxNzc5MDkwMjY0NDczNixcImlQXCI6XCIxMjcuMC4wLjFcIixcInNlcmlhbFZlcnNpb25VSURcIjoyNCxcInR5cGVcIjpcIlVTRVJOQU1FXCIsXCJ1VUlEXCI6MTIxNjMxNzc5MDkwMjY0NDczNixcInVzZXJOYW1lXCI6XCJhZG1pblwifSIsIm5iZiI6MTU3ODgyNzczNCwiVG9rZW5QYXlsb2FkQWJzIjoie1wiSVBcIjpcIjEyNy4wLjAuMVwiLFwiVVVJRFwiOjEyMTYzMTc3OTA5MDI2NDQ3MzYsXCJpUFwiOlwiMTI3LjAuMC4xXCIsXCJzZXJpYWxWZXJzaW9uVUlEXCI6MjQsXCJ0eXBlXCI6XCJVU0VSTkFNRVwiLFwidVVJRFwiOjEyMTYzMTc3OTA5MDI2NDQ3MzYsXCJ1c2VyTmFtZVwiOlwiYWRtaW5cIn0iLCJleHAiOjE1Nzg4Mjc3NDQsImlhdCI6MTU3ODgyNzczNCwianRpIjoiMTIxNjMxNzc5MDkwMjY0NDczNiJ9.t4po2hDvtyTKX-47Eh8Wf8dwKZcV8uWA3ZQftEelO7s";
         try {
-//            String s = JSONObject.toJSONString(admin);
-//            log.info(s);
-//            String userNameJwtToken = jwtToken.createUserNameJwtToken(admin);
-//            log.info(userNameJwtToken);
-//            jwtToken.getTokenPayloadAbs(userNameJwtToken);
-            jwtToken.getTokenPayloadAbs(token);
-            String tokenUUID = JwtToken.getTokenUUID(token);
-            System.out.println(tokenUUID);
+////            String s = JSONObject.toJSONString(admin);
+////            log.info(s);
+////            String userNameJwtToken = jwtToken.createUserNameJwtToken(admin);
+////            log.info(userNameJwtToken);
+////            jwtToken.getTokenPayloadAbs(userNameJwtToken);
+////            jwtToken.getTokenPayloadAbs(token);
+//            String tokenUUID = JwtToken.getTokenUUID(token);
+//            System.out.println(tokenUUID);
         } catch (Exception e) {
             e.printStackTrace();
         }
