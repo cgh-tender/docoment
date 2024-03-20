c6d20b6628535e82ffb26bad74357fa54571c6ba
cgh-tender <late_tender@163.com>
2024-03-21 00:58:28 +0800

    1
    1

--- v3-admin-vite-main/src/layouts/components/Footer/index.vue
+++ v3-admin-vite-main/src/layouts/components/Footer/index.vue
@@ -3,13 +3,17 @@ const VITE_APP_TITLE = import.meta.env.VITE_APP_TITLE
 </script>
 
 <template>
-  <el-footer class="layout-footer">MIT © 2021-PRESENT {{ VITE_APP_TITLE }}</el-footer>
+  <el-footer>
+    <el-card w-full class="layout-footer">
+      <el-text type="info">MIT © 2021-PRESENT {{ VITE_APP_TITLE }}</el-text>
+    </el-card>
+  </el-footer>
 </template>
 
 <style lang="scss" scoped>
 .layout-footer {
   width: 100%;
-  min-height: 50px;
+  min-height: 20px;
   display: flex;
   align-items: center;
   justify-content: center;
--- v3-admin-vite-main/src/views/permission/resource.vue
+++ v3-admin-vite-main/src/views/permission/resource.vue
@@ -16,7 +16,54 @@ const searchData = computed<GetBaseUserTableData>(() => {
     pageSize: paginationData.pageSize
   }
 })
-const data = ref([])
+const data = ref<SelectOption[]>([
+  {
+    value: "1",
+    label: "1",
+    children: [
+      {
+        value: "1",
+        label: "1"
+      }
+    ]
+  },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" },
+  { value: "1", label: "1" }
+])
 const handleNodeClick = (data: SelectOption) => {
   console.log(data)
 }
@@ -24,41 +71,80 @@ const getTableData = () => {}
 </script>
 
 <template>
-  <div v-loading="loading">
+  <el-container v-loading="loading">
+    <el-header>
+      <el-card shadow="never">
+        <el-form ref="searchFormRef" :inline="true" :model="searchData">
+          <el-form-item prop="username" label="用户名">
+            <el-input v-model="searchData.username" placeholder="请输入" />
+          </el-form-item>
+          <el-form-item prop="phone" label="手机号">
+            <el-input v-model="searchData.phone" placeholder="请输入" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" :icon="Search" @click="getTableData">查询</el-button>
+            <el-button :icon="Refresh" @click="resetSearch">重置</el-button>
+            <el-button :icon="ZoomIn" @click="handleAdd">新增</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </el-header>
     <el-container>
-      <el-header>
-        <el-card shadow="never">
-          <el-form ref="searchFormRef" :inline="true" :model="searchData">
-            <el-form-item prop="username" label="用户名">
-              <el-input v-model="searchData.username" placeholder="请输入" />
-            </el-form-item>
-            <el-form-item prop="phone" label="手机号">
-              <el-input v-model="searchData.phone" placeholder="请输入" />
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" :icon="Search" @click="getTableData">查询</el-button>
-              <el-button :icon="Refresh" @click="resetSearch">重置</el-button>
-              <el-button :icon="ZoomIn" @click="handleAdd">新增</el-button>
-            </el-form-item>
-          </el-form>
-        </el-card>
-      </el-header>
-      <el-container>
-        <el-aside width="200px">
+      <el-scrollbar>
+        <el-aside>
           <el-card>
-            <el-tree style="max-width: 600px" :data="data" @node-click="handleNodeClick" />
+            <el-tree :data="data" @node-click="handleNodeClick" />
           </el-card>
         </el-aside>
-        <el-container>
-          <el-main>
-            <el-card>
-              <el-tree style="max-width: 600px" :data="data" @node-click="handleNodeClick" />
-            </el-card>
-          </el-main>
-        </el-container>
+      </el-scrollbar>
+
+      <el-container>
+        <el-main>
+          <el-card></el-card>
+        </el-main>
       </el-container>
     </el-container>
-  </div>
+  </el-container>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-container .el-header {
+  height: auto;
+}
+
+.el-container .el-card {
+  height: 100%;
+}
+
+.el-container .el-aside {
+  padding: 20px 10px 10px 20px;
+  height: 100%;
+}
+
+.el-container .el-main {
+  padding: 20px 20px 10px 10px;
+  height: 100%;
+}
+
+/* 全局样式表 */
+.el-tree {
+  background-color: yellow;
+  font-size: 14px;
+  height: auto;
+}
+
+.el-tree .el-tree-node__content {
+  padding: 10px;
+  margin: 2px;
+  cursor: pointer;
+}
+
+.el-tree .el-tree-node.is-expanded .el-tree-node__children {
+  background: #fff;
+}
+
+.scrollbar-custom .el-scrollbar__wrap {
+  width: 100%;
+  height: 100%;
+}
+</style>
